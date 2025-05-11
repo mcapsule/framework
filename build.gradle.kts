eeeaefa2f1d27af78a1bbb96cb454054b367ece8
ma <jojorambo17@gmail.com>
2025-05-11 12:01:03 +0800

    Add Spring Data JPA and PostgreSQL dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,10 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    runtimeOnly("org.postgresql:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
